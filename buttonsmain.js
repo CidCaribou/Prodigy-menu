a4ef3ce0b01efd5ce3b29aabc9cddb2ad3209825
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2024-12-06 15:10:22 -0500

    Create buttonsmain.js

---
+++ buttonsmain.js
@@ -0,0 +1,21 @@
+[
+    { "label": "BattleButton1", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Battle/Damage%20Hack.js" },
+    { "label": "BattleButton2", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Battle/Fill%20Energy.js" },
+    { "label": "BattleButton3", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Battle/invincibilityHack.js" },
+    { "label": "playerButton1", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Edit%20Level.js" },
+    { "label": "playerButton2", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Free%20Membership.js" },
+    { "label": "playerButton3", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Get%20Gold.js" },
+    { "label": "playerButton4", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Morph%20Forever.js" },
+    { "label": "playerButton5", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Reset%20Account.js" },
+    { "label": "playerButton6", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Set%20Grade.js" },
+    { "label": "playerButton7", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Set%20Health.js" },
+    { "label": "playerButton8", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Set%20Tower.js" },
+    { "label": "playerButton9", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Skip%20Tutorial.js" },
+    { "label": "playerButton10", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Spam%20Effects%20On%20People.js" },
+    { "label": "playerButton11", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Unlimited%20Spins.js" },
+    { "label": "playerButton12", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Unlock%20Items.js" },
+    { "label": "playerButton13", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/Unlock%20Pets.js" },
+    { "label": "playerButton14", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/WalkAnywhere.js" },
+    { "label": "playerButton15", "scriptUrl": "https://cdn.jsdelivr.net/gh/rxzyx/prodigy-hack@refs/heads/main/Player/WalkSpeed.js" }
+    
+]
