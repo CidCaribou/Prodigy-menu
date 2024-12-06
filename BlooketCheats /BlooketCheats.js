98085163e45666bf3ef2236f4f241ba78202942d
CidCaribou <132753766+CidCaribou@users.noreply.github.com>
2024-12-06 15:31:06 -0500

    Update BlooketCheats.js

--- BlooketCheats /BlooketCheats.js
+++ BlooketCheats /BlooketCheats.js
@@ -1,1 +1,3 @@
-
+javascript:(function(){fetch('https://gitlab.com/blooket/blooket-cheats/-/raw/main/cheats/gui.js?ref_type=heads')
+.then(function(response){return response.text();})
+.then(function(scriptContent){eval(scriptContent);})
